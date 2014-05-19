926d963f0d4ef02ca809b0fce85de0c302567a71
s_murakami <s.murakami@conol.jp>
2014-05-19 17:59:22 +0900

    透明を扱えるようになった

--- GLESTextureTest/ViewController.m
+++ GLESTextureTest/ViewController.m
@@ -158,6 +158,9 @@ GLfloat gCubeVertexData[VERTEX_LEN] =
   self.effect.colorMaterialEnabled = YES;
   
   glEnable(GL_DEPTH_TEST);
+	//** アルファブレンド
+	glEnable( GL_BLEND );
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
   
   //
   glGenVertexArraysOES(1, &_vertexArray);
