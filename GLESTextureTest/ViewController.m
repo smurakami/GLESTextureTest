c0b7c9f3066bf253b816364e3d208a34755e463e
s_murakami <s.murakami@conol.jp>
2014-05-20 12:25:34 +0900

    クラス化成功！！

--- GLESTextureTest/ViewController.m
+++ GLESTextureTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "GLSprite.h"
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
@@ -32,6 +33,8 @@ static GLfloat gCubeVertexData[VERTEX_LEN] =
 
 @property (strong, nonatomic) EAGLContext *context;
 @property (strong, nonatomic) GLKBaseEffect *effect;
+
+@property (strong, nonatomic) GLSprite * sprite;
 
 - (void)setupGL;
 - (void)tearDownGL;
@@ -102,36 +105,9 @@ static GLfloat gCubeVertexData[VERTEX_LEN] =
 	glEnable( GL_BLEND );
 	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
   
-  //
-  glGenVertexArraysOES(1, &_vertexArray);
-  glBindVertexArrayOES(_vertexArray);
+  // 物体生成
+  _sprite = [[GLSprite alloc] initWithEffect:self.effect];
   
-  glGenBuffers(1, &_vertexBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(gCubeVertexData), gCubeVertexData, GL_STATIC_DRAW);
-  
-  glEnableVertexAttribArray(GLKVertexAttribPosition);
-  glVertexAttribPointer(GLKVertexAttribPosition,
-                        3, GL_FLOAT, GL_FALSE,
-                        VERTEX_ELEM_LEN * sizeof(GLfloat), BUFFER_OFFSET(0));
-  
-  glEnableVertexAttribArray(GLKVertexAttribNormal);
-  glVertexAttribPointer(GLKVertexAttribNormal,
-                        3, GL_FLOAT, GL_FALSE,
-                        VERTEX_ELEM_LEN * sizeof(GLfloat), BUFFER_OFFSET(sizeof(GLfloat)*3));
-  
-  glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-  glVertexAttribPointer(GLKVertexAttribTexCoord0,
-                        2, GL_FLOAT, GL_FALSE,
-                        VERTEX_ELEM_LEN * sizeof(GLfloat), BUFFER_OFFSET(sizeof(GLfloat)*6));
-  
-  glBindVertexArrayOES(0);
-  
-  NSURL *imageURL = [[NSBundle mainBundle]
-                     URLForResource:@"moyashi" withExtension:@"png"];
-  _texInfo = [GLKTextureLoader
-              textureWithContentsOfURL:imageURL
-              options:nil error:NULL];
 }
 
 - (void)tearDownGL
@@ -171,15 +147,9 @@ static GLfloat gCubeVertexData[VERTEX_LEN] =
   glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
-  glBindVertexArrayOES(_vertexArray);
   
   // Render the object with GLKit
-  self.effect.texture2d0.enabled = GL_TRUE;
-  self.effect.texture2d0.name = _texInfo.name;
-  self.effect.texture2d0.target = _texInfo.target;
-  [self.effect prepareToDraw];
-  
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, VERTEX_NUM);
+  [self.sprite glkView:view drawInRect:rect];
 }
 
 @end
