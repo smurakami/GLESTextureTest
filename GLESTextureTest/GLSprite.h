cd0225cab20c880e4fd694e14d8ee2c251361133
s_murakami <s.murakami@conol.jp>
2014-05-20 11:58:15 +0900

    GLSpriteクラスを作成

---
+++ GLESTextureTest/GLSprite.h
@@ -0,0 +1,13 @@
+//
+//  GLSprite.h
+//  GLESTextureTest
+//
+//  Created by 村上 晋太郎 on 2014/05/20.
+//  Copyright (c) 2014年 村上 晋太郎. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLSprite : NSObject
+
+@end
---
+++ GLESTextureTest/GLSprite.m
@@ -0,0 +1,19 @@
+//
+//  GLSprite.m
+//  GLESTextureTest
+//
+//  Created by 村上 晋太郎 on 2014/05/20.
+//  Copyright (c) 2014年 村上 晋太郎. All rights reserved.
+//
+// ================
+//     GLSprite
+// ================
+//
+// OpenGLESの3D空間上に絵を張り付けるクラスです。
+//
+
+#import "GLSprite.h"
+
+@implementation GLSprite
+
+@end
